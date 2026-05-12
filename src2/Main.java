5ba9b82d7abba8b9e1cbe210a36cf143e8e7d8f0
denizsarpyazicioglu <denizsarp45@gmail.com>
2026-05-12 16:31:13 +0300

    GUI eklendi

--- src2/Main.java
+++ src2/Main.java
@@ -14,5 +14,53 @@ public class Main{
 
         JTextArea textArea = new JTextArea();
         textArea.setFont(new Font(config.getFontName(), Font.PLAIN, config.getFontSize()));
+
+        JLabel statusLabel = new JLabel("Character number: 0");
+
+        model.addObserver(text -> statusLabel.setText("Character number: " + text.length()));
+
+        JPanel buttonPanel = new JPanel();
+        JButton btnType = new JButton("Confirm Text");
+        JButton btnUndo = new JButton("Undp");
+        JButton btnFont = new JButton("Increase size of the font (18)");
+        JButton btnIterate = new JButton("List rows (Console)");
+
+        buttonPanel.add(btnType);
+        buttonPanel.add(btnUndo);
+        buttonPanel.add(btnFont);
+        buttonPanel.add(btnIterate);
+
+        btnType.addActionListener(e -> {
+            TypeCommand cmd = new TypeCommand(model, textArea.getText());
+            commandManager.executeCommand(cmd);
+            System.out.println("Text has been updated!");
+        });
+
+        btnUndo.addActionListener(e -> {
+            commandManager.undo();
+            textArea.setText(model.getText());
+            System.out.println("Last operation has been reversed!");
+        });
+
+        btnFont.addActionListener(e -> {
+            System.out.println("Iteration on rows!");
+            String[] linesList = textArea.getText().split("\n");
+            LineIterator iterator = new LineIterator(Arrays.asList(linesList));
+
+            while(iterator.hasNext()){
+                System.out.println("Line: " + iterator.hasNext());
+            }
+        });
+
+        frame.setLayout(new BorderLayout());
+        frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.NORTH);
+        frame.add(statusLabel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+
+
+
     }
+
 }
